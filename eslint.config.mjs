dd191793ee73b833a15fab453f53ee21aa1eeade
YEO Sugyeong <ssyeo1204@gmail.com>
2026-06-21 19:08:11 +0900

    chore: prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,27 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  prettier,
   {
     rules: {
       // 변수
       "prefer-const": "error",
       "no-var": "error",
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
 
       // 코드 품질
       "no-console": ["warn", { allow: ["warn", "error"] }],
-      "eqeqeq": ["error", "always"],
+      eqeqeq: ["error", "always"],
       "no-duplicate-imports": "error",
 
       // TypeScript
