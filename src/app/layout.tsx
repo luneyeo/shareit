af8c90a8b6c2b40b2c3be5a5f2dcc1d66e080802
YEO Sugyeong <ssyeo1204@gmail.com>
2026-06-24 16:31:02 +0900

    refactor: global css 파일 임포트 경로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@/shared/styles/global.css";
 
 const pretendard = localFont({
   src: "../shared/assets/font/PretendardVariable.woff2",
