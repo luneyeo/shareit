92341fa01ddd1a1178429341253adb8aad763a0f
YEO Sugyeong <ssyeo1204@gmail.com>
2026-07-01 18:17:01 +0900

    refactor: cn 함수 화살표 함수 형식으로 변경

--- src/shared/utils/cn.ts
+++ src/shared/utils/cn.ts
@@ -1,5 +1,5 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 /**
  * 조건부 클래스명을 병합하고 Tailwind CSS 충돌을 해결합니다.
@@ -11,6 +11,6 @@ import { twMerge } from 'tailwind-merge';
  * cn('px-2 py-1', isActive && 'bg-blue-500')
  * cn('px-2', 'px-4') // → 'px-4'
  */
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
