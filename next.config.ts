b2eebfa53275e805721c47554a3f3aea4a157523
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-07-09 00:25:48 +0900

    [Refactor/#30] 같은 경로 내 동일한 다이나믹 라우트 이름 구분 (#31)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
     return config;
   },
