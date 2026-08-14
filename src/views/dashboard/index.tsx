6d72397a479076b6c83edf1c14f667901020d380
YEO Sugyeong <ssyeo1204@gmail.com>
2026-08-14 13:22:03 +0900

    Feat: 그룹 드롭다운 메뉴 배치

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,3 +1,13 @@
+import GroupDropdown from "@/features/dashboard/ui/GroupDropdown";
+import type { Group } from "@/features/dashboard/types";
+
+// TODO: 데이터 계층 연동 전까지 사용하는 임시 목업 데이터.
+const GROUPS: Group[] = [
+  { id: "g1", name: "송파구 공주들" },
+  { id: "g2", name: "강남 러너스" },
+  { id: "g3", name: "한강 자전거" },
+];
+
 /**
  * 사용자 대시보드 페이지 컴포넌트
  *
@@ -6,5 +16,9 @@
  * export default DashboardPage
  */
 export function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <div className="flex flex-col gap-5 p-5">
+      <GroupDropdown groups={GROUPS} currentGroupId={GROUPS[0].id} />
+    </div>
+  );
 }
