fc70f674e0522fe3d3a2845b02c6338c8c548f27
YEO Sugyeong <ssyeo1204@gmail.com>
2026-08-21 18:53:42 +0900

    Refactor: lottie 애니메이션을 index 배럴로 관리

---
+++ src/shared/assets/lottie/index.ts
@@ -0,0 +1,3 @@
+export { default as noGroupAnimation } from "./lottie-no-group.json";
+export { default as noProductAnimation } from "./lottie-no-product.json";
+export { default as noDataAnimation } from "./lottie-no-data.json";
