1eb48d37a73de9c9eb7cbbaf256e4255ae2e669f
YEO Sugyeong <ssyeo1204@gmail.com>
2026-08-23 17:28:14 +0900

    Feat: 그룹 관리 컴포넌트 배럴에 추가

--- src/features/mypage/ui/index.ts
+++ src/features/mypage/ui/index.ts
@@ -1,2 +1,4 @@
 export { default as MyPageSection } from "./MyPageSection";
 export { default as MyPageMenuItem } from "./MyPageMenuItem";
+export { default as GroupFilterTabs } from "./group/GroupFilterTabs";
+export { default as GroupListItem } from "./group/GroupListItem";
