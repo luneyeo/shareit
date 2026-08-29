0216694bccdcf3c3d5c9d6ce51aecd4c235f5907
YEO Sugyeong <ssyeo1204@gmail.com>
2026-08-29 17:56:31 +0900

    Style: Prettier 포맷 미준수 파일 2개 정리

--- src/shared/assets/icons/tab/index.ts
+++ src/shared/assets/icons/tab/index.ts
@@ -1,3 +1,3 @@
-export { default as IcTabDashboard } from './ic-dashboard.svg';
-export { default as IcTabHome } from './ic-home.svg';
-export { default as IcTabMy } from './ic-my.svg';
+export { default as IcTabDashboard } from "./ic-dashboard.svg";
+export { default as IcTabHome } from "./ic-home.svg";
+export { default as IcTabMy } from "./ic-my.svg";
